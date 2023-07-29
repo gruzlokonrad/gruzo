84ae90f9fb1a2a1d1a59c8decf27a61abb905b75
Konrad Gruzło <gruzlokonrad@gmail.com>
2023-07-29 22:44:59 +0200

    update google tag manager

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    NEXT_PUBLIC_GOOGLE_TAG_MANAGER: "GTM-MRKPTZ2",
-    MONGODB_URI: "mongodb+srv://project3_database:pass123@cluster0.axyp0.mongodb.net/adomi"
+    NEXT_PUBLIC_GOOGLE_TAG_MANAGER: "GTM-56CQ7C83",
+    MONGODB_URI: "mongodb+srv://project3_database:pass123@cluster0.axyp0.mongodb.net/gruzo"
   }
 }
 
