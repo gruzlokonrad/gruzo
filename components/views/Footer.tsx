e019d757b725ff8d772c9cbe3fb0f08b9e58684f
Konrad Gruzło <gruzlokonrad@gmail.com>
2023-09-22 12:34:49 +0200

    update footer style

--- components/views/Footer.tsx
+++ components/views/Footer.tsx
@@ -9,27 +9,32 @@ import { useResize } from '@/hooks/useResize';
 type ParagraphType = { children: React.ReactNode, className?: string }
 
 export const Paragraph = ({ children, className }: ParagraphType) => (
-  <p className={tailwindClassMerge(`font-normal text-sm ${className}`)}>{children}</p>
+  <p className={tailwindClassMerge(`font-normal text-xl ${className}`)}>{children}</p>
 )
 
 export const Footer = () => {
   const { isDesktop } = useResize();
 
   return (
-    <footer className="py-20 px-6 lg:flex lg:justify-evenly border border-t-1">
-      <div className="">
-        <h2 className={tailwindClassMerge(`header-2 underline lg:mt-0`)}>Gruzo.dev</h2>
+    <footer className="
+      pt-6 pb-20 px-6 
+      flex flex-col lg:flex-row 
+      items-center lg:items-start lg:justify-evenly
+      border border-t-1
+    ">
+      <div className="w-72 lg:w-auto my-12">
+        <h2 className={tailwindClassMerge(`header-1 text-3xl mb-10 lg:mb-12 lg:mt-0`)}>gruzo.dev</h2>
+        <div className='my-4'>
+          <Paragraph className='underline'>hello@gruzo.dev</Paragraph>
+          <Paragraph className=''>+48 513 950 004</Paragraph>
+        </div>
         <div className='my-4'>
           <Paragraph>2023 © <span className='underline'>gruzo.dev</span>.</Paragraph>
           <Paragraph>Wszelkie prawa zastrzezone</Paragraph>
         </div>
-        <div className='my-4'>
-          <Paragraph className='underline'>hello@gruzo.dev</Paragraph>
-          <Paragraph>+48 513 950 004</Paragraph>
-        </div>
       </div>
-      <div className='mt-10 lg:mt-0'>
-        <div className='flex gap-10 my-4 lg:mt-0 lg:mb-12'>
+      <div className='w-72 lg:w-auto'>
+        <div className='flex gap-10 mb-6 lg:mb-12'>
           <FontAwesomeIcon icon={faFacebookSquare} size={'3x'} />
           <FontAwesomeIcon icon={faLinkedin} size={'3x'} />
         </div>
@@ -43,7 +48,7 @@ export const Footer = () => {
           <ul>
             {menuItems.map((item, index) => {
               return (
-                <li key={item.desktopName} className='my-1'>{item.desktopName}</li>
+                <li key={item.desktopName} className='text-lg my-1'>{item.desktopName}</li>
               )
             })}
           </ul>
